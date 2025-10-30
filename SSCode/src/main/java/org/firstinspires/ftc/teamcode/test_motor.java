b36dae21f5bcdc9bb99ff869cad31e12b6b854a7
Adm <glevse@yandex.com>
2025-10-30 10:49:52 +0300

    Initial commit

---
+++ SSCode/src/main/java/org/firstinspires/ftc/teamcode/test_motor.java
@@ -0,0 +1,34 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@Autonomous(name = "test_motor", group = "Concept")
+public class test_motor extends LinearOpMode {
+
+    @Override
+    public void runOpMode() {
+        DcMotor myMotor = null;
+        myMotor = hardwareMap.get(DcMotor.class, "myMotor");
+        myMotor.setDirection(DcMotor.Direction.FORWARD);
+        myMotor.setPower(0);
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            myMotor.setPower(0.7);
+
+            telemetry.addData("Status", "Running");
+            telemetry.addData("Motor Power", "%.2f", myMotor.getPower());
+            telemetry.update();
+        }
+
+        myMotor.setPower(0);
+        telemetry.addData("Status", "Stopped");
+        telemetry.update();
+    }
+}
